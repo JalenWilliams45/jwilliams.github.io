b61c98ac7f3f5e69b46ce3362d3b0b17d441242b
jwilliams45 <jcwilliams23@my.waketech.edu>
2025-04-11 11:16:30 -0400

    Create gradebook.js

---
+++ gradebook.js
@@ -0,0 +1,11 @@
+console.log("gradebook.js is loaded");
+
+function addStudentRow(studentName, grades) {
+  console.log("addStudentRow called with:", studentName, grades);
+  // This function will be filled in future assignments
+}
+
+function calculateAverages() {
+  console.log("calculateAverages called");
+  // This function will be filled in future assignments
+}
